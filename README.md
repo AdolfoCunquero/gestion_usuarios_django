8c6d99bcf0ca47ca4e02a47bbbcd797c5564abd0
AdolfoCunquero <adolfocunquero.077@gmail.com>
2021-09-09 01:29:39 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Gestion de usuaios con Django
+
---
+++ gestion_usuarios/gestion_usuarios/settings/local.py
@@ -0,0 +1,27 @@
+from .base import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': get_secret("DB_NAME"),
+        'USER': get_secret("DB_USER"),
+        'PASSWORD': get_secret("DB_PASSWORD"),
+        'HOST': 'localhost',
+        'PORT':'5432'
+    }
+}
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.2/howto/static-files/
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR.child('static')]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR.child('media')
